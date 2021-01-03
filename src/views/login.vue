d1fe5d7e32e96fa58da2feb16c48811365c34fe0
threkeyofsun <nattawade.inala@g.swu/ac/th>
2021-01-03 12:17:34 +0700

    master

--- src/views/login.vue
+++ src/views/login.vue
@@ -26,9 +26,7 @@
           </div>
         </div>
 
-        <!-- <button v-on:click="isHidden = true">Hide the text below</button>
-  <button v-on:click="isHidden = !isHidden">Toggle hide and show</button>
-      <h1 v-if="!isHidden">Hide me on click event!</h1> -->
+    
         <log />
       </div>
     </div>
